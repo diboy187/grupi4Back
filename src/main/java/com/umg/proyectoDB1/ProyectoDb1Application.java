02b27df05e1775a54de39ec8e504fa41892e2f8d
Diego187 <diboy1872@gmail.com>
2022-09-27 22:09:09 -0600

    subiendo

---
+++ src/main/java/com/umg/proyectoDB1/ProyectoDb1Application.java
@@ -0,0 +1,13 @@
+package com.umg.proyectoDB1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/proyectoDB1/ProyectoDb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.proyectoDB1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
