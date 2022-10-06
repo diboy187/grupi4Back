548dda3ba256c04d7e1f6e5a2dc4856fe182d9ba
Diego187 <diboy1872@gmail.com>
2022-10-05 23:36:16 -0600

    arreglo departamento2

--- src/main/java/com/umg/proyectoDB1/repository/DepartamentoRepository.java
+++ src/main/java/com/umg/proyectoDB1/repository/DepartamentoRepository.java
@@ -3,5 +3,5 @@ package com.umg.proyectoDB1.repository;
 import com.umg.proyectoDB1.entity.Departamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DepartamentoEntity extends JpaRepository<Departamento, Integer> {
+public interface DepartamentoRepository extends JpaRepository<Departamento, Integer> {
 }
