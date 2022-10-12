e1771989481c9e33ffc5361162c63a35de372bd9
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-10-12 12:44:30 -0600

    cambio login

--- src/main/java/com/umg/proyectoDB1/repository/UsuarioRepository.java
+++ src/main/java/com/umg/proyectoDB1/repository/UsuarioRepository.java
@@ -3,9 +3,11 @@ package com.umg.proyectoDB1.repository;
 import com.umg.proyectoDB1.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
 
+    public List<Usuario> findByUsuarioAndPassword(String user, String password);
 }
--- src/main/java/com/umg/proyectoDB1/service/LoginService.java
+++ src/main/java/com/umg/proyectoDB1/service/LoginService.java
@@ -3,11 +3,13 @@ import com.umg.proyectoDB1.entity.Login;
 import com.umg.proyectoDB1.entity.Usuario;
 import com.umg.proyectoDB1.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 
 @RestController
@@ -30,9 +32,32 @@ public class LoginService {
         if(credentials.getUser().equals("1") && credentials.getPass().equals("admin")){
             usuario.setUsuario(credentials.getUser());
             usuario.setPassword(credentials.getPass());
+
         }
         return  usuario;
     }
+
+
+
+    @PostMapping(path = "/autenticacion2")
+    private ResponseEntity<List<Usuario>> login(@RequestBody Login credenciales){
+
+        Usuario usuario = new Usuario();
+        usuario.setPassword(credenciales.getPass());
+        usuario.setUsuario(credenciales.getUser());
+        System.out.println("usuario -> " + usuario.getUsuario());
+        if(usuario.getUsuario().isEmpty() || usuario.getPassword().isEmpty()) {
+            return ResponseEntity.noContent().build();
+
+        }else if(usuario.getUsuario().equals("admin") && usuario.getPassword().equals("admin")){
+
+            return ResponseEntity.ok().build();
+        }
+        else{
+            return  ResponseEntity.ok(usuarioRepository.findByUsuarioAndPassword(usuario.getUsuario(),usuario.getPassword()));
+        }
+    }
+
 
 
     private static String MD5(String entrada){
