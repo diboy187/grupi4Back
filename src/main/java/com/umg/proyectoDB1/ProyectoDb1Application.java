4fc7b9aa0e19cc821432a37647d0e358a269a458
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-06-20 15:49:47 -0600

    inicio

--- src/main/java/com/umg/proyectoDB1/ProyectoDb1Application.java
+++ src/main/java/com/umg/proyectoDB1/ProyectoDb1Application.java
@@ -1,8 +1,6 @@
 package com.umg.proyectoDB1;
 
-import com.umg.proyectoDB1.repository.ClienteRepository;
-import com.umg.proyectoDB1.service.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
